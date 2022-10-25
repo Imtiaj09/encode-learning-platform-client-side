8d9b8599871d9e91dbf58364bc6e690fe9665ec3
Imtiaj09 <ahmimtiaj@gmail.com>
2022-10-25 14:15:11 +0600

    tailwing css and daisyui-tailwind component has been installed

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDbuYfYdRyfHcAZxV5qHUvvI-S4XoPuDMY",
+  authDomain: "encode-c3e0c.firebaseapp.com",
+  projectId: "encode-c3e0c",
+  storageBucket: "encode-c3e0c.appspot.com",
+  messagingSenderId: "849236438393",
+  appId: "1:849236438393:web:c5456dd0c03341411093a8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
