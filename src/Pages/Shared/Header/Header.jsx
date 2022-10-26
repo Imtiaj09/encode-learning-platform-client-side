177ca9ec2bcc53c7d263ff1bf138af76fe511a04
Imtiaj09 <ahmimtiaj@gmail.com>
2022-10-27 01:55:48 +0600

    responsive navbar has been added

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,7 +1,50 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div>This is header</div>;
+  return (
+    <header className="text-gray-600 body-font">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <Link
+          to="/"
+          className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            className="w-10 h-10 text-white p-2 bg-gray-900 rounded-full"
+            viewBox="0 0 24 24"
+          >
+            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+          </svg>
+          <span className="ml-3 text-xl">EnCode</span>
+        </Link>
+        <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+          <Link to="/" className="mr-5 hover:text-gray-900">
+            Home
+          </Link>
+
+          <Link to="/course" className="mr-5 hover:text-gray-900">
+            Course
+          </Link>
+          <Link to="/faq" className="mr-5 hover:text-gray-900">
+            FAQ
+          </Link>
+          <Link to="/blog" className="mr-5 hover:text-gray-900">
+            Blog
+          </Link>
+
+          <a href="#" className="mr-5 hover:text-gray-900">
+            Login
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
